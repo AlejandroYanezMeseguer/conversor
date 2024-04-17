921b1366ef7163c6c92587aeccff462f7f7b5a44
AlejandroYanezMeseguer <ayanezmeseguer@danielcastelao.org>
2024-04-17 13:11:49 +0200

    terminador

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Conversor miConversor = new Conversor();
+        miConversor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
